d01f9566a891ec2f000b6c359ecccea83320a476
SkyLined <github@skylined.nl>
2016-12-09 17:05:49 +0100

    Redirect Blog post 2016-12-03 to  2016-12-05

---
+++ Static/20161203001.html/index.php
@@ -0,0 +1,15 @@
+<?php header('Location: http://blog.skylined.nl/20161205001.html', true, 301); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Location" content="http://blog.skylined.nl/20161205001.html"/>
+    <meta http-equiv="refresh" content="0; url=http://blog.skylined.nl/20161205001.html">
+    <title>Move to http://blog.skylined.nl/20161205.html</title>
+    <script>
+      location.replace("http://blog.skylined.nl/20161205001.html");
+    </script>
+  </head>
+  <body>
+    Moved to <a href="http://blog.skylined.nl/20161205001.html">http://blog.skylined.nl/20161205001.html</a>.
+  </body>
+</html>
